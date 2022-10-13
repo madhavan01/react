e157b6b4a41dfeab67261118bc9e5f15ec4a04ce
madhavan01 <73778506+madhavan01@users.noreply.github.com>
2022-10-13 10:44:32 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
